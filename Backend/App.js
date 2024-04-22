62d19ec3b1574810b862989344c6ade64b6b3385
ShaguftaAmreen <132890125+ShaguftaAmreen@users.noreply.github.com>
2024-04-22 07:59:28 +0530

    Update App.js

--- Backend/App.js
+++ Backend/App.js
@@ -1,1 +1,35 @@
 
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+const mainRoutes = require('./routes/routes');
+
+const whitelist = ['http://localhost:3001'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.use('/', mainRoutes);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT || 3000}`);
+});
